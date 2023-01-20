4390d17d884690525eda85238878de76c0be5ad2
A194542 <ricardo.jimenez@dekra.com>
2023-01-20 17:19:47 +0100

    Peliculas app

---
+++ peliculas_app/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'package:peliculas_app/screens/screens.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Peliculaes',
+      initialRoute: 'home',
+      routes: {
+        'home': (_) => const HomeScreen(),
+        'details': (_) => const DetailsScreen()
+      },
+      theme: ThemeData.light().copyWith(
+        appBarTheme: const AppBarTheme(
+          color: Colors.indigo,
+          elevation: 0,
+        ),
+      ),
+    );
+  }
+}
---
+++ peliculas_app/lib/screens/home_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas_app/widgets/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Películas en cine'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search_outlined),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: const [
+            CardSwiper(),
+            MovieSlider(),
+          ],
+        ),
+      ),
+    );
+  }
+}
